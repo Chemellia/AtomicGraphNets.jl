284fa9add82da5451f9dafbc2c3a89056440a4d1
Rachel Kurchin <rkurchin@mit.edu>
2020-03-31 09:17:37 -0400

    fixed nonexistent electronegativity warnings

--- featurize.jl
+++ featurize.jl
@@ -38,13 +38,14 @@ all_elements = [e.symbol for e in elements[1:max_atno]]
 # leave off noble gases too
 nums_to_skip = [2, 10, 18, 36, 54]
 
-avail_features = ["group", "row", "block", "atomic_mass", "atomic_radius", "van_der_waals_radius", "X"]
+avail_features = ["Z", "group", "row", "block", "atomic_mass", "atomic_radius", "van_der_waals_radius", "X"]
 global categorical_features = ["group", "row", "block"]
 global categorical_feature_vals = Dict("group"=>1:18, "row"=>1:6, "block"=>["s", "p", "d", "f"])
 
 # compile data... (and skip noble gases)
 pt = pyimport("pymatgen.core.periodic_table")
-atom_data = Dict(i => pt.Element(all_elements[i]) for i in 1:max_atno)
+atom_data = [pt.Element(all_elements[i]) for i in 1:max_atno if !(i in nums_to_skip)]
+all_elements = [all_elements[i] for i in 1:max_atno if !(i in nums_to_skip)]
 
 py"""
 def isnone(x):
@@ -55,13 +56,13 @@ def isnone(x):
 global atom_data_df = DataFrame(sym = all_elements)
 for feature in avail_features
     if feature=="block"
-        feature_vals = ["a" for i in 1:max_atno]
-    elseif feature in ["group", "row"]
-        feature_vals = missings(Int32, max_atno)
+        feature_vals = ["a" for i in 1:length(all_elements)]
+    elseif feature in ["Z", "group", "row"]
+        feature_vals = missings(Int32, length(all_elements))
     else
-        feature_vals = missings(Float32, max_atno)
+        feature_vals = missings(Float32, length(all_elements))
     end
-    for i in keys(atom_data)
+    for i in 1:length(all_elements)
         feature_val = getproperty(atom_data[i], feature)
         if !py"isnone"(feature_val)
             feature_vals[i] = feature_val
@@ -69,10 +70,6 @@ for feature in avail_features
     end
     global atom_data_df[!, Symbol(feature)] = feature_vals
 end
-
-# drop rows we don't need...
-atom_data_df = atom_data_df[[i for i in 1:max_atno if !(i in nums_to_skip)], :]
-all_elements = [all_elements[i] for i in 1:max_atno if !(i in nums_to_skip)]
 
 # relabel rows for lanthanoids (should be 6 not 8)
 rows = atom_data_df.row

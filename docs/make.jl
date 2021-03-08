896369cc4fb9fe898fc98bd970b0707c3652eef1
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-03-09 00:12:12 +0530

    docs: inital documentation setup
    Add Documenter as a dependency.
    Have git ignore build resources generated by Documenter.
    Set up initial home page of project documentation.
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

---
+++ docs/make.jl
@@ -0,0 +1,14 @@
+using Documenter, AtomicGraphNets
+
+makedocs(
+	sitename = "AtomicGraphNets.jl",
+	modules = [AtomicGraphNets],
+	pages = Any[
+		"Home" => "index.md",
+	]
+)
+deploydocs(
+	repo = "github.com/thazhemadam/AtomicGraphNets.jl.git",
+	target = "build",
+	branch = "gh-pages"
+)

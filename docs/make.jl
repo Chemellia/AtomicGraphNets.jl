4b8029fdd7021fdd7c68235205f2181aa47f6dff
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-03-10 14:45:11 +0530

    docs: make.jl: configure make.jl a little more
    Tweak make.jl's settings a little more, for more flexibility.
    Explicitly specify format, enable linkcheck, allow support for
    commandline arguments to be parsed (if the CI ever needs to be tweaked
    as such), and enable push_preview as well.
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,14 @@
 using Documenter, AtomicGraphNets
 
+# The DOCSARGS environment variable can be used to pass additional arguments to make.jl.
+# This is useful on CI, if you need to change the behavior of the build slightly but you
+# can not change the .travis.yml or make.jl scripts any more (e.g. for a tag build).
+if haskey(ENV, "DOCSARGS")
+    for arg in split(ENV["DOCSARGS"])
+        (arg in ARGS) || push!(ARGS, arg)
+    end
+end
+
 makedocs(
 	sitename = "AtomicGraphNets.jl",
 	modules = [AtomicGraphNets],
@@ -8,10 +17,17 @@ makedocs(
 		"Basic Graph Theory" => "graph_theory.md",
 		"GCNNs" => "gcnns.md",
 		"Comparison with cgcnn.py" => "comparison.md",
-	]
+	],
+	format = Documenter.HTML(
+		# Use clean URLs, unless built as a "local" build
+		prettyurls = !("local" in ARGS),
+		canonical = "https://thazhemadam.github.io/AtomicGraphNets.jl/stable/",	
+	),
+	linkcheck = "linkcheck" in ARGS,
 )
 deploydocs(
 	repo = "github.com/thazhemadam/AtomicGraphNets.jl.git",
 	target = "build",
-	branch = "gh-pages"
+	branch = "gh-pages",
+	push_preview = true
 )

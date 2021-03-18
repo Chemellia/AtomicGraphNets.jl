e1a83a42cf74c546f90a624b4278828f485f26ae
Rachel Kurchin <rkurchin@cmu.edu>
2021-03-18 18:54:09 -0400

    set up function/model index, start fixing docstrings in modules to actually show up properly, but I think this will require a version bump to propagate

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,7 @@
 using Pkg
 Pkg.activate(".")
-using Documenter, AtomicGraphNets
+using Documenter
+using AtomicGraphNets
 
 # The DOCSARGS environment variable can be used to pass additional arguments to make.jl.
 # This is useful on CI, if you need to change the behavior of the build slightly but you
@@ -15,14 +16,16 @@ makedocs(
 	sitename = "AtomicGraphNets.jl",
 	modules = [AtomicGraphNets],
 	pages = Any[
-		"Home" => "index.md",
-		"Basic Graph Theory" => "graph_theory.md",
-		"GCNNs" => "gcnns.md",
-		"Comparison with cgcnn.py" => "comparison.md",
-		"Examples" => Any[
-			"Example 1" => "examples/example_1.md",
-			"Example 2" => "examples/example_2.md",
-		]
+	    "Home"                     => "index.md",
+	    "Basic Graph Theory"       => "graph_theory.md",
+	    "GCNNs"                    => "gcnns.md",
+	    "Comparison with cgcnn.py" => "comparison.md",
+	    "Examples"                 => Any[
+		"Example 1" => "examples/example_1.md",
+		"Example 2" => "examples/example_2.md"],
+            "Functions"                => Any[
+                "Layers"    => "functions/layers.md",
+                "Models"    => "functions/models.md"]
 	],
 	format = Documenter.HTML(
 		# Use clean URLs, unless built as a "local" build

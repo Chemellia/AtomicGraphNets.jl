289d852a45f2d1c3a134d0fe1e8a637fa80e86e1
Rachel Kurchin <rkurchin@cmu.edu>
2021-03-22 16:18:08 -0400

    use Flux.Parallel

--- src/models.jl
+++ src/models.jl
@@ -29,25 +29,17 @@ function build_CGCNN(input_feature_length; num_conv=2, conv_activation=softplus,
     model = Chain(AGNConv(input_feature_length=>atom_conv_feature_length, conv_activation, initW=initW), [AGNConv(atom_conv_feature_length=>atom_conv_feature_length, conv_activation, initW=initW) for i in 1:num_conv-1]..., AGNPool(pool_type, atom_conv_feature_length, pooled_feature_length, pool_width), [Dense(pooled_feature_length, pooled_feature_length, hidden_layer_activation, initW=initW) for i in 1:num_hidden_layers-1]..., Dense(pooled_feature_length, output_length, output_layer_activation, initW=initW))
 end
 
-## TODO: potentially make stuff to automatically "cleave" a bulk + adsorbate structure to build the two slab graph inputs from one
-
-"""
-This is a helper function to the main model builder below. Takes in the inputs and models for the two "parallel" CGCNN-like models at the start of the SGCNN architecture and outputs the concatenated final result.
-"""
-function slab_graph_layer(bulk_graph::AtomGraph, bulk_model, surface_graph::AtomGraph, surface_model)
-    bulk_output = bulk_model(bulk_graph)
-    surface_output = surface_model(surface_graph)
-    vcat(bulk_output, surface_output)
-end
+# copied from https://fluxml.ai/Flux.jl/dev/models/advanced/
+Flux.@functor Join
+Join(combine, paths) = Parallel(combine, paths)
+Join(combine, paths...) = Join(combine, paths)
 
 """
 Build a slab graph network. For now, fixing both "parallel" convolutional paths to have same hyperparams for simplicity. Might relax this later.
-
-TODO: change this to use the new Flux.Parallel construct
 """
 function build_SGCNN(input_feature_length::Integer; num_conv=2, conv_activation=softplus, atom_conv_feature_length=80, pool_type="mean", pool_width=0.1, pooled_feature_length=40, hidden_layer_width=40, num_hidden_layers=3, hidden_layer_activation=softplus, output_layer_activation=identity, output_length=1, initW=glorot_uniform)
     bulk_model = Chain(AGNConv(input_feature_length=>atom_conv_feature_length, conv_activation, initW=initW), [AGNConv(atom_conv_feature_length=>atom_conv_feature_length, conv_activation, initW=initW) for i in 1:num_conv-1]..., AGNPool(pool_type, atom_conv_feature_length, pooled_feature_length, pool_width))
     surface_model = Chain(AGNConv(input_feature_length=>atom_conv_feature_length, conv_activation, initW=initW), [AGNConv(atom_conv_feature_length=>atom_conv_feature_length, conv_activation, initW=initW) for i in 1:num_conv-1]..., AGNPool(pool_type, atom_conv_feature_length, pooled_feature_length, pool_width))
-    model = Chain(graphs->slab_graph_layer(graphs[1], bulk_model, graphs[2], surface_model), Dense(2*pooled_feature_length, hidden_layer_width, hidden_layer_activation, initW=initW), [Dense(hidden_layer_width, hidden_layer_width, hidden_layer_activation, initW=initW) for i in 1:num_hidden_layers-1]..., Dense(hidden_layer_width, output_length, output_layer_activation, initW=initW))
+    model = Chain(Join(vcat, surface_model, bulk_model), Dense(2*pooled_feature_length, hidden_layer_width, hidden_layer_activation, initW=initW), [Dense(hidden_layer_width, hidden_layer_width, hidden_layer_activation, initW=initW) for i in 1:num_hidden_layers-1]..., Dense(hidden_layer_width, output_length, output_layer_activation, initW=initW))
 end
 
